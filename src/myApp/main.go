ea285989456178c325ad52b2f53a1481bfe6d8d4
delfi-pt <shashankvivek.7@gmail.com>
2019-05-25 16:06:06 +0530

    added mux requests

---
+++ src/myApp/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Product struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Qunatity int    `json:"qty"`
+}
+
+var products []Product
+
+func main() {
+	handleRequest()
+}
+
+func handleRequest() {
+	products = append(products, Product{ID: "id1", Name: "Prod1", Qunatity: 2})
+	products = append(products, Product{ID: "id2", Name: "Prod2", Qunatity: 5})
+	r := mux.NewRouter()
+	r.HandleFunc("/products", GetProducts).Methods("GET")
+	r.HandleFunc("/product/{id}", GetProductById).Methods("GET")
+	r.HandleFunc("/product", CreateProduct).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func GetProducts(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(products)
+}
+
+func GetProductById(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range products {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Product{})
+}
+
+func CreateProduct(w http.ResponseWriter, req *http.Request) {
+	// params := mux.Vars(req)
+	var prod Product
+	_ = json.NewDecoder(req.Body).Decode(&prod)
+	products = append(products, prod)
+	json.NewEncoder(w).Encode(&products)
+}
